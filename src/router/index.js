acf522a437660f2c2b8ea3157125866eb027c7cd
szdg <szilagyidavidg@gmail.com>
2025-04-15 12:58:26 +0200

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -22,8 +22,8 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL), // Use process.env.BASE_URL for safety
-    routes,
+    history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 
 export default router;
